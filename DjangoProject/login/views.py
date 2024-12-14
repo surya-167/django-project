2a6690085de4f2a42e9f47ab8fe42f313e76bfb0
surya <65219914+surya-167@users.noreply.github.com>
2024-12-14 11:28:42 +0530

    Update views.py

--- DjangoProject/login/views.py
+++ DjangoProject/login/views.py
@@ -4,4 +4,7 @@ from httpresponses import HttpResponses
 # Create your views here.
 def main(requests):
     template = loader.get_template('login.html')
-    return HttpResponse(template.render())
+    context={img="media/profile-user.png",
+    img2="zzz.jppeg"}
+    
+    return HttpResponse(template.render(),context)
